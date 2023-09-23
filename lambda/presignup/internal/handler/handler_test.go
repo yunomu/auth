e8334587861a1897b800958241a6543f4604323d
Yusuke Nomura <yunomu@gmail.com>
2023-09-23 13:33:16 +0900

    Remove unnecessary test

--- lambda/presignup/internal/handler/handler_test.go
+++ lambda/presignup/internal/handler/handler_test.go
@@ -2,19 +2,8 @@ package handler
 
 import (
 	"testing"
-
-	"github.com/yunomu/auth/lib/userlist"
 )
 
-func TestContainEmail(t *testing.T) {
-	h := NewHandler([]*userlist.User{
-		{Name: "ghi"},
-		{Name: "def"},
-		{Name: "abc"},
-	})
-
-	if !h.containsEmail("abc") {
-		t.Errorf("error")
-		t.Logf("list: %#v", h.userlist)
-	}
+func TestEmpty(t *testing.T) {
+	// do nothing
 }
